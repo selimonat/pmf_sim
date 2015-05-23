d0edee5c26cd29da1d9ae62036a5ca4dfb1afd83
leakp <lea.kampermann@gmail.com>
2015-05-23 17:53:51 +0200

    minor changes in variable names to logfilestructure

--- feargenET_PFfitting_Fitplot.m
+++ feargenET_PFfitting_Fitplot.m
@@ -1,4 +1,4 @@
-function feargenET_PFfitting_Fitplot(subject,Log)
+function feargenET_PFfitting_Fitplot(subject,p)
  x=0:1:100;
  xstimrange=0:11.25:100;
 % collect number of trials per stimulus Level
@@ -9,17 +9,17 @@ minn=5;
 maxn=25;
 
 fig=figure('units','normalized','outerposition',[0 0 0.8 1]);
-for chain=1:size(Log.x,1)
-    
-    dotsize  = Scale(sum(~isnan(Log.xrounded(:,:,chain)),2))*(maxn-minn)+minn;
-    pcorrect = nanmean(Log.xrounded(:,:,chain),2);
-    error    = nanstd(Log.xrounded(:,:,chain),0,2);
+for chain=1:size(p.psi.log.x,1)
+    %dotsize gets scaled by number of trials at this x
+    dotsize  = Scale(sum(~isnan(p.psi.log.xrounded(:,:,chain)),2))*(maxn-minn)+minn;
+    pcorrect = nanmean(p.psi.log.xrounded(:,:,chain),2);
+    error    = nanstd(p.psi.log.xrounded(:,:,chain),0,2);
    
     subplot(2,2,chain)
     plot(x,...
-        PAL_CumulativeNormal([Log.alpha(chain,end) 10.^(Log.beta(chain,end)) Log.gamma(chain,end) Log.lambda(chain,end)],x),'b')
+        PAL_CumulativeNormal([p.psi.log.alpha(chain,end) 10.^(p.psi.log.beta(chain,end)) p.psi.log.gamma(chain,end) p.psi.log.lambda(chain,end)],x),'b')
     hold on;
-    line([Log.alpha(end) Log.alpha(end)],[ -.2 1.2],'Color','y')
+    line([p.psi.log.alpha(end) p.psi.log.alpha(end)],[ -.2 1.2],'Color','y')
     %detect the nonnan entries
     for i =  find(~isnan(pcorrect(:))')
         errorbar(xstimrange(i),pcorrect(i),error(i),'ko','Markersize',dotsize(i));
@@ -34,35 +34,35 @@ for chain=1:size(Log.x,1)
 end
 
 suptitle(sprintf('Fitting Subject %02s',subject))
-folder='C:\Users\onat\Dropbox\feargen_lea\EthnoMaster\DiscriminationTask\pilotedata\';
-saveas(fig,sprintf('%sFit_Subj%02s.png',folder,subject))
-
-% to plot the presented x-values
-nx=mean(d.nxmean,2)
-a
+% folder='C:\Users\onat\Dropbox\feargen_lea\EthnoMaster\DiscriminationTask\pilotedata\';
+% saveas(fig,sprintf('%sFit_Subj%02s.png',folder,subject))
 % 
- x=0:1:100;
- xstimrange=0:11.25:100;
- a0=45;
- b0=10;
- g0=0.1;
- l0=0.02;
-% collect number of trials per stimulus Level
-minn=5;
-maxn=25;
-figure
-plot(x,PAL_CumulativeNormal([a 1/b g l],x),'b')
-hold on;
-plot(x,PAL_CumulativeNormal([a0 1/b0 g0 l0],x),'r')
- for i =  1:length(xstimrange)
-        plot(xstimrange(i),1.2,'ko','Markersize',dotsize(i),'MarkerFaceColor',[0.3 0.3 0.3]);
-        hold on;
- end
- xlim([-5 100])
- ylim([-.1 1.8])
- legend('estimated PF','generating PF','stimulus placement','Location','southeast')
- 
-bar(mean(d.nxmean,2))
-set(gca,'XTick',[1 2 3 4 5 6 7 8 9],'XTickLabel',{'0','11.25','22.5','33.75','45','56.25','67.5','78.75','90'})
+% % to plot the presented x-values
+% nx=mean(d.nxmean,2)
+% a
+% % 
+%  x=0:1:100;
+%  xstimrange=0:11.25:100;
+%  a0=45;
+%  b0=10;
+%  g0=0.1;
+%  l0=0.02;
+% % collect number of trials per stimulus Level
+% minn=5;
+% maxn=25;
+% figure
+% plot(x,PAL_CumulativeNormal([a 1/b g l],x),'b')
+% hold on;
+% plot(x,PAL_CumulativeNormal([a0 1/b0 g0 l0],x),'r')
+%  for i =  1:length(xstimrange)
+%         plot(xstimrange(i),1.2,'ko','Markersize',dotsize(i),'MarkerFaceColor',[0.3 0.3 0.3]);
+%         hold on;
+%  end
+%  xlim([-5 100])
+%  ylim([-.1 1.8])
+%  legend('estimated PF','generating PF','stimulus placement','Location','southeast')
+%  
+% bar(mean(d.nxmean,2))
+% set(gca,'XTick',[1 2 3 4 5 6 7 8 9],'XTickLabel',{'0','11.25','22.5','33.75','45','56.25','67.5','78.75','90'})
 
 
