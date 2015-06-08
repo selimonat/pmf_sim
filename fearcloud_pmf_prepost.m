65f50a62ca4c6413c381cef5ce5ea2da3e925505
leakp <lea.kampermann@gmail.com>
2015-06-08 20:21:46 +0200

    plots PMFs Pre-Post, CS+ vs CS- and saves it in ph05/figures folder

---
+++ fearcloud_pmf_prepost.m
@@ -0,0 +1,83 @@
+function fearcloud_pmf_prepost(subjects)
+
+
+global project_path
+
+[pmf]=isn_getPMF(subjects,[1,5]);
+
+x         = 0:1:150;
+cs=0;
+for sub=subjects
+    cs=cs+1;
+    
+    fig(1)=figure('units','normalized','outerposition',[0 0 0.8 .7]);
+    suptitle(sprintf('Fitting Subject %g, Pre-Post',sub));
+    subplot(1,2,1)
+    %Pre vs Post, CSN and CSP in one figure
+    
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,1,1) 10.^(pmf.beta(cs,1,1)) pmf.gamma(cs,1,1) pmf.lambda(cs,1,1)],x),'r','linewidth',2)
+    hold on;
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,2,1) 10.^(pmf.beta(cs,2,1)) pmf.gamma(cs,2,1) pmf.lambda(cs,2,1)],x),'b','linewidth',2)
+    legend('CS+','CS-','location','southeast')
+    title('Pre')
+    xlabel('X (deg)')
+    ylabel('p("different")')
+    
+    %%%%%
+    subplot(1,2,2)
+    
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,1,2) 10.^(pmf.beta(cs,1,2)) pmf.gamma(cs,1,2) pmf.lambda(cs,1,2)],x),'r','linewidth',2)
+    hold on;
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,2,2) 10.^(pmf.beta(cs,2,2)) pmf.gamma(cs,2,2) pmf.lambda(cs,2,2)],x),'b','linewidth',2)
+    legend('CS+','CS-','location','southeast')
+    title('Post')
+    xlabel('X (deg)')
+    ylabel('p("different")')
+      
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    save_path = sprintf('%sfigures/%s_prepost.png',isn_GetPath(sub,5),mfilename);
+    hgexport(fig(1),save_path);
+    save_path = sprintf('%sfigures/%s_prepost.eps',isn_GetPath(sub,5),mfilename);
+    hgexport(fig(1),save_path);
+    print(sprintf('%sfigures/%s_prepost_print',isn_GetPath(sub,5),mfilename),'-dpng')
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    fig(2)=figure('units','normalized','outerposition',[0 0 0.8 .7]);
+    suptitle(sprintf('Fitting Subject %g, CS+ vs CS-',sub));
+    
+    subplot(1,2,1)
+    %CS+ left, CS- right figure - pre vs post in one figure.
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,1,1) 10.^(pmf.beta(cs,1,1)) pmf.gamma(cs,1,1) pmf.lambda(cs,1,1)],x),'r--','linewidth',2)
+    hold on;
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,1,2) 10.^(pmf.beta(cs,1,2)) pmf.gamma(cs,1,2) pmf.lambda(cs,1,2)],x),'r-','linewidth',2)
+    legend('Pre','Post','location','southeast')
+    title('CS+')
+    xlabel('X (deg)')
+    ylabel('p("different")')
+    %%%%% 
+    subplot(1,2,2)    
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,2,1) 10.^(pmf.beta(cs,2,1)) pmf.gamma(cs,2,1) pmf.lambda(cs,2,1)],x),'b--','linewidth',2)
+    hold on;
+    plot(x,...
+        PAL_CumulativeNormal([pmf.alpha(cs,2,2) 10.^(pmf.beta(cs,2,2)) pmf.gamma(cs,2,2) pmf.lambda(cs,2,2)],x),'b-','linewidth',2)
+    legend('Pre','Post','location','southeast')
+      title('CS-')
+    xlabel('X (deg)')
+    ylabel('p("different")')
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    save_path = sprintf('%sfigures/%s_CS.png',isn_GetPath(sub,5),mfilename);
+    hgexport(fig(2),save_path);
+    save_path = sprintf('%sfigures/%s_CS.eps',isn_GetPath(sub,5),mfilename);
+    hgexport(fig(2),save_path);
+    print(sprintf('%sfigures/%s_CS_print',isn_GetPath(sub,5),mfilename),'-dpng')
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+end
