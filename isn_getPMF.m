94b5fdfaf4eba96d55aae0b7cfbb388c5868e6cd
leakp <lea.kampermann@gmail.com>
2015-08-24 16:18:10 +0200

    complete version with all four parameters

--- isn_getPMF.m
+++ isn_getPMF.m
@@ -1,4 +1,6 @@
 function [pmf]=isn_getPMF(subject,run)
+global project_path
+project_path='C:\Users\onat\Google Drive\EthnoMaster\data\';
 cs=0;
 cr=0;
 for ns=subject
@@ -13,6 +15,11 @@ for ns=subject
             pmf.seAlpha(cs,chain,cr)=p.psi.log.seAlpha(chain,end);
             pmf.beta(cs,chain,cr)=p.psi.log.beta(chain,end);
             pmf.seBeta(cs,chain,cr)=p.psi.log.seBeta(chain,end);
+            pmf.gamma(cs,chain,cr)=p.psi.log.gamma(chain,end);
+            pmf.seGamma(cs,chain,cr)=p.psi.log.seGamma(chain,end);
+            pmf.lambda(cs,chain,cr)=p.psi.log.lambda(chain,end);
+            pmf.seLambda(cs,chain,cr)=p.psi.log.seLambda(chain,end);
+         
             %
             pmf.indsubject(cs,chain,cr)=ns;
             pmf.indrun(cs,chain,cr)=cr;
